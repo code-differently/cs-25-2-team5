0c850e9da0fc892ae6132fa7cf2d899c7fb901ff
Boyce007 <94003665+Boyce007@users.noreply.github.com>
2025-09-17 16:33:38 -0400

    Daniel Boyce feat:adds Gradle build for project (#2)
    * feat:adds gradle build for project
    
    * chore:adds dev container to project
    
    ---------
    
    Co-authored-by: Daniel Boyce <danielboyce@Daniels-MacBook-Air.local>
    Co-authored-by: Daniel <Boyce007>

---
+++ cbl/src/test/java/com/team5/cbl/cbl/CblApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team5.cbl.cbl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CblApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
