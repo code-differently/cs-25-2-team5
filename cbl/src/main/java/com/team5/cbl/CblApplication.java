9434f0851df17ec908ff2c3c5f382b7a465603c4
Daniel <Boyce007>
2025-09-18 17:43:24 +0000

    chore:added spotless apply and build is successful

--- cbl/src/main/java/com/team5/cbl/CblApplication.java
+++ cbl/src/main/java/com/team5/cbl/CblApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CblApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CblApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(CblApplication.class, args);
+  }
 }
