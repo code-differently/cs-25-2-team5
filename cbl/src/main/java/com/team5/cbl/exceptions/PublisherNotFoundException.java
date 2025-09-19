5f1cb5ebbf6898fdfecf1a8595790864dcf4b2e6
Calvin Robinson <cwrobinson252@gmail.com>
2025-09-19 11:14:10 -0400

    Feat: OOP Project - CBL Project - Character Class & Unit Test - Updated (#11)
    * feat: added character class and character test files
    
    * feat: removed publisher strings from class and test files

--- cbl/src/main/java/com/team5/cbl/exceptions/PublisherNotFoundException.java
+++ cbl/src/main/java/com/team5/cbl/exceptions/PublisherNotFoundException.java
@@ -1,7 +1,7 @@
 package com.team5.cbl.exceptions;
 
 public class PublisherNotFoundException extends IllegalArgumentException {
-    public PublisherNotFoundException(String message) {
-        super(message);
-    }
+  public PublisherNotFoundException(String message) {
+    super(message);
+  }
 }
