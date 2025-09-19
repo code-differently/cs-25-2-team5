02ff1679ad963ae6f7136800c5c559876abfe964
Talia Crockett <tmcrockett252@gmail.com>
2025-09-19 20:49:43 +0000

    fix(filter): adds and improves filtering logic, applies spotlessApply

--- cbl/src/main/java/com/team5/cbl/cbl_app/objects/ComicLibrary.java
+++ cbl/src/main/java/com/team5/cbl/cbl_app/objects/ComicLibrary.java
@@ -5,71 +5,89 @@
 
 package com.team5.cbl.cbl_app.objects;
 
+import com.team5.cbl.cbl_app.enums.Genre;
+import com.team5.cbl.cbl_app.exceptions.ComicNotFoundException;
 import java.util.List;
 import java.util.stream.Collectors;
-import com.team5.cbl.cbl_app.exceptions.ComicNotFoundException;
-
 
 /**
- *
  * @author vscode
  */
 public class ComicLibrary {
 
-    private List<Comic> comics;
+  private List<Comic> comics;
 
-
-    public ComicLibrary(List<Comic> comics) {
-        this.comics = comics;
-
-    }
-
-  public List<Comic> filterByComicTitle(String title) {
-      List<Comic> comicsByFilter =   comics.stream()
-      .filter(comic-> comic.getTitle().equals(title))
-      .collect(Collectors.toList());
-      if (comicsByFilter.isEmpty()) {
-          throw new ComicNotFoundException("Comic title not found");
-      }
-      return comicsByFilter;
+  public ComicLibrary(List<Comic> comics) {
+    this.comics = comics;
   }
 
-    public List<Comic> filterByPublisher() {
-        return null;
+  public List<Comic> filterByComicTitle(String title) {
+    List<Comic> comicsByFilter =
+        comics.stream()
+            .filter(comic -> comic.getTitle().equals(title))
+            .collect(Collectors.toList());
+    if (comicsByFilter.isEmpty()) {
+      throw new ComicNotFoundException("Comic title not found");
     }
+    return comicsByFilter;
+  }
 
-    public List<Comic> filterByCharacter() {
-        return null;
+  public List<Comic> filterByPublisher(Publisher publisher) {
+    List<Comic> comicsByFilter =
+        comics.stream()
+            .filter(comic -> comic.getPublisher().equals(publisher))
+            .collect(Collectors.toList());
+    if (comicsByFilter.isEmpty()) {
+      throw new ComicNotFoundException("Publisher not found");
     }
+    return comicsByFilter;
+  }
 
-    public List<Comic> filterByGenre() {
-        return null;
+  public List<Comic> filterByCharacter(String heroName) {
+    List<Comic> comicsByFilter =
+        comics.stream()
+            .filter(comic -> comic.getLeadingCharacter().getHeroName().equals(heroName))
+            .collect(Collectors.toList());
+    if (comicsByFilter.isEmpty()) {
+      throw new ComicNotFoundException("Character not found");
     }
+    return comicsByFilter;
+  }
 
-    public List<Comic> filterByCreator() {
-        return null;
+  public List<Comic> filterByGenre(Genre genre) {
+    List<Comic> comicsByFilter =
+        comics.stream()
+            .filter(comic -> comic.getGenre().contains(genre))
+            .collect(Collectors.toList());
+    if (comicsByFilter.isEmpty()) {
+      throw new ComicNotFoundException("Genre not found");
     }
+    return comicsByFilter;
+  }
 
-    public List<Comic> getComics() {
-        return comics;
+  public List<Comic> filterByCreator(String name) {
+    List<Comic> comicsByFilter =
+        comics.stream()
+            .filter(comic -> comic.getHeadWriter().getName().equals(name))
+            .collect(Collectors.toList());
+    if (comicsByFilter.isEmpty()) {
+      throw new ComicNotFoundException("Creator not found");
     }
+    return comicsByFilter;
+  }
 
-    // sorts the lists by the grade and returns the list of comics
-    public List<Comic> getRankings() {
-        return null;
-    }
+  public List<Comic> getComics() {
+    return comics;
+  }
 
-    // Adds a new comic to library
-    public void addComic(Comic comic) {
+  // sorts the lists by the grade and returns the list of comics
+  public List<Comic> getRankings() {
+    return null;
+  }
 
-    }
+  // Adds a new comic to library
+  public void addComic(Comic comic) {}
 
-    // removes a 
-    public void removeComic(Comic comic) {
-
-    }
-
-
-
-
+  // removes a
+  public void removeComic(Comic comic) {}
 }
