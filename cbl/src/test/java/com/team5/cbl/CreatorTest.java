dfda37aa6588f10ad78fdf4c5e8eeb20f77a7a44
Joy Brown <jxbrown252@gmail.com>
2025-09-19 14:17:28 +0000

    test(creator): add unit tests for Creator constructor and getters

---
+++ cbl/src/test/java/com/team5/cbl/CreatorTest.java
@@ -0,0 +1,33 @@
+package com.team5.cbl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CreatorTest {
+
+    @Test
+    @DisplayName("Constructor sets fields; getters return values")
+    void constructorAndGetters() {
+        Creator creator = new Creator("Joy Brown", 39);
+        assertEquals("Joy Brown", creator.getName());
+        assertEquals(39, creator.getAge());
+    }
+
+    @Test
+    @DisplayName("Different creators have independent values")
+    void multipleCreators() {
+        Creator key = new Creator("Key", 18);
+        Creator zach = new Creator("Zach", 17);
+        Creator pyes = new Creator("Pyes", 15);
+
+        assertEquals("Key", key.getName());
+        assertEquals(18, key.getAge());
+
+        assertEquals("Zach", zach.getName());
+        assertEquals(17, zach.getAge());
+
+        assertEquals("Pyes", pyes.getName());
+        assertEquals(15, pyes.getAge());
+    }
+}
