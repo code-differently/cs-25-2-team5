7d4a85258f3f54f6afab314a9a5bb7a001adb6b4
Daniel <Boyce007>
2025-09-19 17:09:01 +0000

    feat:adds comic library class

---
+++ cbl/src/main/java/com/team5/cbl/cbl_app/objects/ComicLibrary.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.team5.cbl.cbl_app.objects;
+
+import java.util.List;
+
+/**
+ *
+ * @author vscode
+ */
+public class ComicLibrary {
+
+    private List<Comic> comics;
+
+
+    public ComicLibrary(List<Comic> comics) {
+        this.comics = comics;
+
+    }
+
+
+    public List<Comic> filterByInput(String input) {
+
+        return null;
+
+    }
+
+    // sorts the lists by the grade and returns the list of comics
+    public List<Comic> getRankings() {
+        return null;
+    }
+
+    // Adds a new comic to library
+    public void addComic(Comic comic) {
+
+    }
+
+    // removes a 
+    public void removeComic(Comic comic) {
+
+    }
+
+
+
+
+}
