c0d126a6289e63ae4b5ab341c6d3e33c6a751754
Kaldigi252-ops <cwrobinson252@gmail.com>
2025-09-19 21:16:25 +0000

    feat: added addComic and removeComic syntax to ComicLibrary

--- cbl/src/main/java/com/team5/cbl/cbl_app/objects/ComicLibrary.java
+++ cbl/src/main/java/com/team5/cbl/cbl_app/objects/ComicLibrary.java
@@ -5,71 +5,76 @@
 
 package com.team5.cbl.cbl_app.objects;
 
+import com.team5.cbl.cbl_app.exceptions.ComicNotFoundException;
 import java.util.List;
 import java.util.stream.Collectors;
-import com.team5.cbl.cbl_app.exceptions.ComicNotFoundException;
-
 
 /**
- *
  * @author vscode
  */
 public class ComicLibrary {
 
-    private List<Comic> comics;
+  private List<Comic> comics;
 
-
-    public ComicLibrary(List<Comic> comics) {
-        this.comics = comics;
-
-    }
-
-  public List<Comic> filterByComicTitle(String title) {
-      List<Comic> comicsByFilter =   comics.stream()
-      .filter(comic-> comic.getTitle().equals(title))
-      .collect(Collectors.toList());
-      if (comicsByFilter.isEmpty()) {
-          throw new ComicNotFoundException("Comic title not found");
-      }
-      return comicsByFilter;
+  public ComicLibrary(List<Comic> comics) {
+    this.comics = comics;
   }
 
-    public List<Comic> filterByPublisher() {
-        return null;
+  public List<Comic> filterByComicTitle(String title) {
+    List<Comic> comicsByFilter =
+        comics.stream()
+            .filter(comic -> comic.getTitle().equals(title))
+            .collect(Collectors.toList());
+    if (comicsByFilter.isEmpty()) {
+      throw new ComicNotFoundException("Comic title not found");
     }
+    return comicsByFilter;
+  }
 
-    public List<Comic> filterByCharacter() {
-        return null;
+  public List<Comic> filterByPublisher() {
+    return null;
+  }
+
+  public List<Comic> filterByCharacter() {
+    return null;
+  }
+
+  public List<Comic> filterByGenre() {
+    return null;
+  }
+
+  public List<Comic> filterByCreator() {
+    return null;
+  }
+
+  public List<Comic> getComics() {
+    return comics;
+  }
+
+  // sorts the lists by the grade and returns the list of comics
+  public List<Comic> getRankings() {
+    return null;
+  }
+
+  // Adds a new comic to library
+  public void addComic(Comic comic) {
+    if (comic != null && !comics.contains(comic)) {
+      comics.add(comic);
+      System.out.println("Added comic: " + comic.getTitle());
+    } else {
+      System.out.println(
+          "Comic already exists or is null: " + (comic != null ? comic.getTitle() : "null"));
     }
+  }
 
-    public List<Comic> filterByGenre() {
-        return null;
+  // removes a comic from library
+  public void removeComic(Comic comic) {
+    if (comic != null && comics.contains(comic)) {
+      comics.remove(comic);
+      System.out.println("Removed comic: " + comic.getTitle());
+    } else {
+      System.out.println(
+          "Comic not found or is null: " + (comic != null ? comic.getTitle() : "null"));
     }
-
-    public List<Comic> filterByCreator() {
-        return null;
-    }
-
-    public List<Comic> getComics() {
-        return comics;
-    }
-
-    // sorts the lists by the grade and returns the list of comics
-    public List<Comic> getRankings() {
-        return null;
-    }
-
-    // Adds a new comic to library
-    public void addComic(Comic comic) {
-
-    }
-
-    // removes a 
-    public void removeComic(Comic comic) {
-
-    }
-
-
-
-
+  }
 }
