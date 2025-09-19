a8f151afa6fe5ccb8b8cff228e1058afea2a4ed0
Talia Crockett <tmcrockett252@gmail.com>
2025-09-19 14:22:21 +0000

    feat: adds Exceptions folder and files for classes

---
+++ cbl/src/main/java/com/team5/cbl/exceptions/ComicNotFoundException.java
@@ -0,0 +1,7 @@
+package com.team5.cbl.exceptions;
+
+public class ComicNotFoundException extends IllegalArgumentException {
+    public ComicNotFoundException(String message) {
+        super(message);
+    }
+}
