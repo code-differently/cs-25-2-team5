6e0ac0a672c633abce15a8a75a63a60960e7a17e
ethanhillman <echillman252@gmail.com>
2025-10-16 19:28:31 +0000

    refactor: remove unsupported setter methods from EventModel

--- event-planner/api/demo/src/main/java/com/api/demo/models/EventModel.java
+++ event-planner/api/demo/src/main/java/com/api/demo/models/EventModel.java
@@ -13,26 +13,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class EventModel {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
-	 public void setId(Long test_ID) {
-        throw new UnsupportedOperationException("Unimplemented method 'setId'");
-    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
-	private String title;
-	 public void setTitle(String test_TITLE) {
-		throw new UnsupportedOperationException("Unimplemented method 'setTitle'");
-	}
-
-	private String description;
-	 public void setDescription(String test_DESCRIPTION) {
-        throw new UnsupportedOperationException("Unimplemented method 'setDescription'");
-    }
-
-	private LocalDateTime startTime;
-	 public void setStartTime(LocalDateTime test_START_TIME) {
-		throw new UnsupportedOperationException("Unimplemented method 'setStartTime'");
-	}
+    private String title;
+    
+    private String description;
+    
+    private LocalDateTime startTime;
 }
 
