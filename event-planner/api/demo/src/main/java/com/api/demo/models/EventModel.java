116c95b6a184b1d0365547ff20345c1ceba93ed7
ethanhillman <echillman252@gmail.com>
2025-10-16 19:16:54 +0000

    feat: add EventModel and its tests with setter and getter methods

--- event-planner/api/demo/src/main/java/com/api/demo/models/EventModel.java
+++ event-planner/api/demo/src/main/java/com/api/demo/models/EventModel.java
@@ -1,7 +1,38 @@
 package com.api.demo.models;
-import jakarta.persistence.Entity;
-@Entity
-public class EventModel {
-    
+import java.time.LocalDateTime;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class EventModel {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	 public void setId(Long test_ID) {
+        throw new UnsupportedOperationException("Unimplemented method 'setId'");
+    }
+
+	private String title;
+	 public void setTitle(String test_TITLE) {
+		throw new UnsupportedOperationException("Unimplemented method 'setTitle'");
+	}
+
+	private String description;
+	 public void setDescription(String test_DESCRIPTION) {
+        throw new UnsupportedOperationException("Unimplemented method 'setDescription'");
+    }
+
+	private LocalDateTime startTime;
+	 public void setStartTime(LocalDateTime test_START_TIME) {
+		throw new UnsupportedOperationException("Unimplemented method 'setStartTime'");
+	}
 }
+
