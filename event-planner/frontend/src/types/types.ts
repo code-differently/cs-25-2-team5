f2ac0b8ee27a3fc96c40ea8bb1b44b5941c56739
Talia Crockett <tmcrockett252@gmail.com>
2025-10-16 15:02:06 +0000

    feat: adds Id to Event Interface

--- event-planner/frontend/src/types/types.ts
+++ event-planner/frontend/src/types/types.ts
@@ -1,4 +1,5 @@
 export interface Event {
+    id: string;
     title: string;
     location: string;
     owner: string;
@@ -7,24 +8,28 @@ export interface Event {
 
 export const events: Event[] = [
     {
+        id: "1",
         title: "hello",
         location: "Here",
         owner: "New",
         time: "1",
     },
     {
+        id: "2",
         title: "hello2",
         location: "Here",
         owner: "New",
         time: "6",
     },
     {
+        id: "3",
         title: "hello2",
         location: "Here",
         owner: "New",
         time: "7",
     },
     {
+        id: "4",
         title: "hello3",
         location: "Here",
         owner: "New",
