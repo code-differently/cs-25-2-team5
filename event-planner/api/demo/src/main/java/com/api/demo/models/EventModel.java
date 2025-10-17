8c24b8e86924bc46bb2ed4d265d9b46193629da7
ethanhillman <echillman252@gmail.com>
2025-10-17 09:58:14 -0400

    Create EventModel with tests and update package import (#38)
    * feat:creates event model
    
    * chore:updates package import
    
    * feat: add EventModel and its tests with setter and getter methods
    
    * refactor: remove unsupported setter methods from EventModel

---
+++ event-planner/api/demo/src/main/java/com/api/demo/models/EventModel.java
@@ -0,0 +1,26 @@
+package com.api.demo.models;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class EventModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+    
+    private String description;
+    
+    private LocalDateTime startTime;
+}
+
