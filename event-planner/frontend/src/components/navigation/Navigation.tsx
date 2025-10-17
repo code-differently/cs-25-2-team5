bdb154fcf933fdad5a6ef04b37f0eab30512f52c
Talia Crockett <tmcrockett252@gmail.com>
2025-10-17 02:32:58 +0000

    feat(style): adds implementation on navigation where login button disappears when active, update style and color palette

--- event-planner/frontend/src/components/navigation/Navigation.tsx
+++ event-planner/frontend/src/components/navigation/Navigation.tsx
@@ -1,24 +1,34 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import './Navigation.css';
 
 const Navigation: React.FC = () => {
+  const location = useLocation();
+  const isLoginPage = location.pathname === '/login';
+
   return (
+    <>
+    <div className="navigation-logo">
+      <a href="/">
+        <img src="/evynt-logo.png" alt="Evynt Logo" />
+      </a>
+    </div>
     <nav className="navigation-bar">
-      <ul className="navigation-top-menu">
-        <li>
-          <a href="#">Discover</a>
-        </li>
-        <li>
-          <a href="#">About Us</a>
-        </li>
-      </ul>
-      
-      <div className="auth-buttons">
-        <Link to="/login">Login</Link>
-        {/* <Link to="/register">Register</Link>  */} {/* Add this back when the Register */}
-      </div>
-    </nav>
+        <ul className="navigation-top-menu">
+          <li>
+            <a href="#">Discover</a> {/* Directs you to the community events */}
+          </li>
+          <li>
+            <a href="#">About Us</a> {/* Placeholder or can actually make an about us page */}
+          </li>
+        </ul>
+
+        <div className="auth-buttons">
+          {!isLoginPage && <Link to="/login">Login</Link>}
+          {/* <Link to="/register">Register</Link>  */} {/* Add this back when the Register */}
+        </div>
+      </nav>
+    </>
   );
 };
 
