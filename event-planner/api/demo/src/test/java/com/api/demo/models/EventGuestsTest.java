8a0525e6dcaf0537e4fb6552acd37845719548d3
Daniel Boyce <danielboyce@Daniels-MacBook-Air.local>
2025-10-17 16:14:20 -0400

    test:adds unit tests for the event guest class

--- event-planner/api/demo/src/test/java/com/api/demo/models/EventGuestsTest.java
+++ event-planner/api/demo/src/test/java/com/api/demo/models/EventGuestsTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Nested;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.LocalDateTime;
