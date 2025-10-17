5b8aaedfccf5b801c5e972231b4ed76995fd4fc6
Daniel Boyce <danielboyce@Daniels-MacBook-Air.local>
2025-10-17 15:53:12 -0400

    feat:adds key for many to many connection

---
+++ event-planner/api/demo/src/main/java/com/api/demo/models/EventGuestKey.java
@@ -0,0 +1,18 @@
+package com.api.demo.models;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Embeddable
+@Data
+@AllArgsConstructor
+public class EventGuestKey implements Serializable{
+    @Column(name = "event_id")
+    private Long eventId;
+    @Column(name = "guest_id")
+    private Long guestId;
+}
