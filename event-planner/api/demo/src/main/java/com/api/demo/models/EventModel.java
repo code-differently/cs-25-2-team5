27e67d27a423af34111156909fef0cd36d525d2d
Boyce007 <94003665+Boyce007@users.noreply.github.com>
2025-10-19 12:29:08 -0400

    Daniel Boyce Updates User model with organized events and creates jpa repository for manging users and events (#47)
    * chore :adds dependency to configure dotenv files
    
    * chore:changes app description in build gradle
    
    * feat:adds dockerfile to deploy app
    
    * feat:adds event organizer subclass and mapping for inheriteance in spring
    
    * feat:removes event organizer class and adds organized event to base user class
    
    * feat:adds user repo to project and non null annotations to required fields
    
    * feat:adds jpa methods in repos that will be needed for services
    
    * chore:adds comment to explain lazy loading
    
    ---------
    
    Co-authored-by: Daniel Boyce <danielboyce@Daniels-MacBook-Air.local>

--- event-planner/api/demo/src/main/java/com/api/demo/models/EventModel.java
+++ event-planner/api/demo/src/main/java/com/api/demo/models/EventModel.java
@@ -1,9 +1,11 @@
 package com.api.demo.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
@@ -31,4 +33,7 @@ public class EventModel {
 
   @OneToMany(mappedBy = "event")
   private Set<EventGuest> eventGuests;
+  // lazy loading to prevent fetching organizer details unless needed to avoid circular references
+  @ManyToOne(fetch = FetchType.LAZY)
+  private User organizer;
 }
---
+++ event-planner/api/demo/src/main/java/com/api/demo/repos/UserRepo.java
@@ -0,0 +1,13 @@
+package com.api.demo.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.api.demo.models.User;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+
+    User findById(int id);
+
+    User findByEmail(String email);
+
+}
