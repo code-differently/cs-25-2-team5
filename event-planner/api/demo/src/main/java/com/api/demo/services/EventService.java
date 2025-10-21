f433c81fc2f99daed90891de26d4faff796c35d1
Daniel Boyce <danielboyce@Daniels-MacBook-Air.local>
2025-10-21 10:10:06 -0400

    chore:changes event service imports

--- event-planner/api/demo/src/main/java/com/api/demo/services/EventService.java
+++ event-planner/api/demo/src/main/java/com/api/demo/services/EventService.java
@@ -1,10 +1,7 @@
 package com.api.demo.services;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.api.demo.models.EventModel;
 import com.api.demo.repos.EventModelRepo;
 @Service
