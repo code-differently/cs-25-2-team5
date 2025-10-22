a5b26a9a3870838e4fc7933d46cbb7269fef764a
Daniel Boyce <danielboyce@Daniels-MacBook-Air.local>
2025-10-22 10:40:06 -0400

    spotlesscheck and test for eventguest service

--- event-planner/api/demo/src/main/java/com/api/demo/controllers/UserController.java
+++ event-planner/api/demo/src/main/java/com/api/demo/controllers/UserController.java
@@ -6,10 +6,8 @@ import com.api.demo.models.EventModel;
 import com.api.demo.models.User;
 import com.api.demo.services.UserService;
 import jakarta.validation.Valid;
-
 import java.util.HashSet;
 import java.util.Set;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
--- event-planner/api/demo/src/main/java/com/api/demo/repos/UserRepository.java
+++ event-planner/api/demo/src/main/java/com/api/demo/repos/UserRepository.java
@@ -1,11 +1,8 @@
 package com.api.demo.repos;
 
 import com.api.demo.models.User;
-
-import java.util.List;
 import java.util.Optional;
 import java.util.Set;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
