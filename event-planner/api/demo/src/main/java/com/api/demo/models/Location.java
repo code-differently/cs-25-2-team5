023e32421687582a6330ae19c83442ea07de8682
ethanhillman <echillman252@gmail.com>
2025-10-23 17:49:47 +0000

    refactor: remove Lombok @Data annotation and replace with explicit getters and setters for clarity

--- event-planner/api/demo/src/main/java/com/api/demo/models/Location.java
+++ event-planner/api/demo/src/main/java/com/api/demo/models/Location.java
@@ -2,23 +2,86 @@ package com.api.demo.models;
 
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Embeddable
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Location {
+
     private Double latitude;
     private Double longitude;
-
+    
     private String city;
     private String state;
     private String country;
     private String zipCode;
     private String formattedAddress;
     private String inputAddress;
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public String getFormattedAddress() {
+        return formattedAddress;
+    }
+
+    public String getInputAddress() {
+        return inputAddress;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public void setFormattedAddress(String formattedAddress) {
+        this.formattedAddress = formattedAddress;
+    }
+
+    public void setInputAddress(String inputAddress) {
+        this.inputAddress = inputAddress;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
 
     public boolean hasCoordinates() {
         return latitude != null && longitude != null;
