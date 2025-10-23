7e5ef504153a2354ba08a98a594cf1be674239bb
Daniel Boyce <danielboyce@Daniels-MacBook-Air.local>
2025-10-23 16:43:11 -0400

    feat:fullstack connection

--- event-planner/api/demo/src/main/java/com/api/demo/controllers/EventController.java
+++ event-planner/api/demo/src/main/java/com/api/demo/controllers/EventController.java
@@ -4,12 +4,14 @@ import com.api.demo.models.EventModel;
 import com.api.demo.services.EventService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/events")
+@CrossOrigin("*")
 public class EventController {
 
   @Autowired private EventService eventService;
