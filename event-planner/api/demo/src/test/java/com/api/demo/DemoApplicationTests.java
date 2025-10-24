730458ce3527e8fec984a776cb981fa945650feb
Daniel Boyce <danielboyce@Daniels-MacBook-Air.local>
2025-10-24 10:00:36 -0400

    chore:updates test envorinment

--- event-planner/api/demo/src/test/java/com/api/demo/DemoApplicationTests.java
+++ event-planner/api/demo/src/test/java/com/api/demo/DemoApplicationTests.java
@@ -2,8 +2,10 @@ package com.api.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest(classes = DemoApplication.class)
+@TestPropertySource(locations = "classpath:application-test.properties")
 class DemoApplicationTests {
 
   @Test
