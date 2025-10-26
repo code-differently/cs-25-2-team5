78377ac7bcc3489d5f9327572c01a73b0901a44a
Daniel Boyce <danielboyce@Daniels-MacBook-Air.local>
2025-10-26 17:20:04 -0400

    chore:adds cross origin to event guest controller

--- event-planner/api/demo/src/main/java/com/api/demo/controllers/EventGuestController.java
+++ event-planner/api/demo/src/main/java/com/api/demo/controllers/EventGuestController.java
@@ -11,6 +11,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/event-guests")
+@CrossOrigin
 public class EventGuestController {
 
   private final EventGuestService eventGuestService;
@@ -61,7 +63,6 @@ public class EventGuestController {
       @PathVariable Long organizerId, @RequestBody CreateEventWithGuestsRequest request) {
     EventModel createdEvent = DTOConverter.mapToModel(request);
     EventModel savedEvent = eventGuestService.createEventWithGuests(organizerId, createdEvent, request.getGuestEmails());
-
     return ResponseEntity.status(HttpStatus.CREATED).body(savedEvent);
   }
 
